2868555656be1a3011315e57ae7c134a83e85d65
Gustavo Henrique <gu.bat98@gmail.com>
2021-03-04 10:30:41 -0300

    test: parking specific tests

--- __tests__/api/parking.test.js.js
+++ __tests__/api/parking.test.js.js
@@ -1,6 +1,7 @@
 const app = require('./../../app')
 const supertest = require('supertest')
 const request = supertest(app)
+let reserveNumber = null
 
 it('gets the root endpoint', async done => {
   const response = await request.get('/')
@@ -38,5 +39,59 @@ it('create parking reservation', async done => {
   expect(response.body).toHaveProperty('enteredAt')
   expect(response.body).toHaveProperty('leftAt')
   expect(response.body).toHaveProperty('time')
+
+  // Set reserve number for next test
+  reserveNumber = response.body.reserveNumber
   done()
 })
+
+it('block a registered car but not paid yet to leave', async done => {
+  const response = await request.put(`/api/parkings/${reserveNumber}/out`)
+
+  expect(response.status).toBe(402)
+
+  expect(response.body).toMatchObject({
+    error: 'Você deve pagar o estacionamento antes de sair.'
+  })
+  done()
+})
+
+it('pay a parking reservation', async done => {
+  const response = await request.put(`/api/parkings/${reserveNumber}/pay`)
+  expect(response.status).toBe(200)
+
+  expect(response.body).toHaveProperty('plate')
+  expect(response.body).toHaveProperty('reserveNumber')
+  expect(response.body).toHaveProperty('paid')
+  expect(response.body).toHaveProperty('left')
+  expect(response.body).toHaveProperty('enteredAt')
+  expect(response.body).toHaveProperty('leftAt')
+  expect(response.body).toHaveProperty('time')
+  done()
+})
+
+it('block paying an already paid parking reservation', async done => {
+  const response = await request.put(`/api/parkings/${reserveNumber}/pay`)
+  expect(response.status).toBe(422)
+
+  expect(response.body).toMatchObject({
+    error: 'Essa reserva já foi paga.'
+  })
+  done()
+})
+
+it('leave a parking reservation', async done => {
+  const response = await request.put(`/api/parkings/${reserveNumber}/out`)
+  expect(response.status).toBe(200)
+
+  expect(response.body).toHaveProperty('plate')
+  expect(response.body).toHaveProperty('reserveNumber')
+  expect(response.body).toHaveProperty('paid')
+  expect(response.body).toHaveProperty('left')
+  expect(response.body).toHaveProperty('enteredAt')
+  expect(response.body).toHaveProperty('leftAt')
+  expect(response.body).toHaveProperty('time')
+
+  done()
+})
+
