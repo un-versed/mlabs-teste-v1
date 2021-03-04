ec8a43e96ece12d24ec4728bf92c3f3c8c24bd6e
Gustavo Henrique <gu.bat98@gmail.com>
2021-03-04 01:05:24 -0300

    fix: root route message

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,10 @@ const router = require('express').Router()
 
 router.use('/api', require('./api'))
 
+// Root route
+router.get('/', (req, res, next) => {
+  return res.json({ title: 'mLabs Teste API v1' })
+})
+
+
 module.exports = router
