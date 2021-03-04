23608f16f5cf55b30544dedfbaeb499581841893
Gustavo Henrique <gu.bat98@gmail.com>
2021-03-04 02:41:52 -0300

    fix: rename joi require

--- services/Validator/index.js
+++ services/Validator/index.js
@@ -1,4 +1,4 @@
-const Joi = require('@hapi/joi')
+const Joi = require('joi')
 const JoiValidationError = require('./../../exceptions/JoiValidationError')
 const options = {
   messages: require('./language')
